fbde42cb4ee82b6f83c12a6f1afdaaa42ceb7cea
michaelnipper3 <118383611+michaelnipper3@users.noreply.github.com>
2025-04-19 22:03:09 -0400

    temp fixes to issues

--- portfolio-very-theme.js
+++ portfolio-very-theme.js
@@ -5,8 +5,8 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
-import "@haxtheweb/scroll-buttons/scroll-buttons.js";
-import "@haxtheweb/simple-cta/simple-cta.js";
+// import "@haxtheweb/scroll-button/scroll-button.js";
+// import "@haxtheweb/simple-cta/simple-cta.js";
 
 /**
  * `portfolio-very-theme`
